8ba31b39726732fa1221de35c5c4214589c1fca4
fancn21th <fancn21th@gmail.com>
2024-06-02 19:42:15 +0800

    📦 NEW: syntax highlight

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-const markdown = `Just a link: www.nasa.gov.`;
+// Did you know you can use tildes instead of backticks for code in markdown? ✨
+const markdown = `Here is some JavaScript code:
+
+~~~js
+console.log('It works!')
+~~~
+`;
 
 function App() {
-  return <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>;
+  return (
+    <Markdown
+      children={markdown}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              children={String(children).replace(/\n$/, "")}
+              language={match[1]}
+              style={dark}
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    />
+  );
 }
 
 export default App;
