0c16f9d86ae6931669bfa83c5f634baaa70f3ff8
fancn21th <fancn21th@gmail.com>
2024-06-06 14:16:09 +0800

    👌 IMPROVE: missing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import Markdown from "react-markdown";
-import headingPlugin from "./headingPlugin";
+import remarkGfm from "remark-gfm";
+import commonMdastNodePlugin from "./commonMdastNodePlugin";
 import "./App.css";
 import md from "./example.md?raw";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { light } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function App() {
-  return <Markdown children={md} remarkPlugins={[headingPlugin]} />;
+  return (
+    <Markdown
+      children={md}
+      remarkPlugins={[remarkGfm, commonMdastNodePlugin]}
+      components={{
+        code(props) {
+          const { children, className, node, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              children={String(children).replace(/\n$/, "")}
+              language={match[1]}
+              style={light}
+            />
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    />
+  );
 }
 
 export default App;
--- src/commonMdastNodePlugin.ts
+++ src/commonMdastNodePlugin.ts
@@ -2,7 +2,7 @@ import { visit } from "unist-util-visit";
 
 export default function plugin() {
   return function (tree) {
-    visit(tree, ["heading", "text"], function (node, _, parent) {
+    visit(tree, ["heading", "listItem", "text"], function (node, _, parent) {
       if (node.type === "text") {
         console.log(parent);
       }
