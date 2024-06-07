48ac45ed63c20c3b2f5fa1f56c9963737829d401
fancn21th <fancn21th@gmail.com>
2024-06-07 08:30:45 +0800

    👌 IMPROVE: only log highlight

--- src/components/markNodePlugin.ts
+++ src/components/markNodePlugin.ts
@@ -6,17 +6,14 @@ export default function plugin(options) {
   const { start, end } = options;
   return function (tree) {
     visit(tree, ["text"], function (node, index, parent) {
-      console.log({ node });
+      // console.log({ node });
 
       const currentStart = cursor;
       const currentEnd = cursor + node.value.length;
 
       cursor = currentEnd;
 
-      // console.log({ cursor, v: node.value });
-
       if (currentStart < end && currentEnd > start) {
-        console.log({ currentStart, currentEnd, start, end, v: node.value });
         parent.data = {
           ...parent.data,
           highlight: true,
